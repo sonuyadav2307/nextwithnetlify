4a3681e33b53609f1f876e3489c12c1c12c7280e
Sonu Yadav <sonu.yadav@creolesstudios.com>
2022-01-04 16:00:35 +0530

    Feature: About page new text added

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
     return (
         <div>
-          <h1>This is about page new</h1>  
+          <h1>This is about pagessss</h1>  
         </div>
     )
 }
